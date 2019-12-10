f729502ef0d4b748f58a4106e31c17912ae89984
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-12-10 23:47:18 +0900

    06_아자아자화이팅

--- storage.c
+++ storage.c
@@ -51,19 +51,21 @@ static void printStorageInside(int x, int y) {
 //int x, int y : cell coordinate to be initialized
 static void initStorage(int x, int y) {
 
-//set all the member variable as an initial value
+	//set all the member variable as an initial value
 	deliverySystem[x][y].building =0;													//??한방에초기화하는 방법  
 	deliverySystem[x][y].room =0;
 	deliverySystem[x][y].cnt =0;
 	deliverySystem[x][y].passwd[0] ='\0';
 
-//and allocate memory to the context pointer
-	deliverySystem[x][y].context = (char*)malloc(MAX_MSG_SIZE*(sizeof(char));			//??char *context를 사용하는 것이 옳은 것인가? 
+	//and allocate memory to the context pointer
+	deliverySystem[x][y].context = (char*)malloc((sizeof(char)*MAX_MSG_SIZE);			//??char *context를 사용하는 것이 옳은 것인가? 문자하나에 char하나? 
+	//메모리가 할당되지 않은 경우: 오류처리 코드 필요                                   //??확인방법 
 	if(deliverySystem[x][y].context == NULL){
-		printf("allocate memory Errors\n");												//메모리가 할당 되지 않은 경우: 오류처리 코드 필요
-		exit(1);  																		//??return -1
+		printf("allocate memory Errors\n");												
+		exit(1);  																		//??return -1: 무슨 종료가 옳은 것일까  
 	}
-	free(deliverySystem[x][y].context); 												// 메모리 반납  
+	//메모리 반납  
+	free(deliverySystem[x][y].context); 												//??생각해보니 여기서는 메모리 반납이 필요 없지 않나  
 }
 
 //get password input and check if it is correct for the cell (x,y)
@@ -73,12 +75,13 @@ static int inputPasswd(int x, int y) {
 	
 	printf(" - input password for (%d, %d) storage : ", x,y);
 	scanf("%4s",&getpassword);
+	//enter로인한 버퍼 비우기  
 	while(getchar() != '\n');															 //??버퍼 비우기 (돈모양 fflush보다 안정강화 버전 )
 	
-//return : 0 - password is matching (마스터비밀번호 또는 저장된 비밀번호)	
+	//return : 0 - password is matching (마스터비밀번호 또는 저장된 비밀번호)	
 	if(strcmp(deliverySystem[x][y].passwd,getpassword)==0 || strcmp(masterPassword,getpassword)==0)   //??마스터 비밀번호 진짜 짜증난다. 		
 		return 0;
-//return : 0 - password is not matching	
+	//return : 0 - password is not matching	
 	else{
 		printf(" -----------> password is wrong!!/n -----------> Failed to extract my package!")
 	} 
@@ -93,15 +96,16 @@ static int inputPasswd(int x, int y) {
 //char* filepath : filepath and name to write                           //??name to write는 무슨 뜻일까  
 int str_backupSystem(char* filepath) {
 	
+	//쓰기 모드로 파일여는 스트림 형성 
 	FILE *fp = fopen(filepath,"w");                                   //??읽고 쓰기 모드 모두다 가능해야 하는 거 아님? 
 
-//return -1 : failed to backup(스트림 생성 실패)	                 //??파일열기 실패도 알려줘야 하나? 이건 어떻게 확인하지ㅋ 진짜 내머리는 장식용  
+	//return -1 : failed to backup(스트림 생성 실패)	                 //??파일열기 실패도 알려줘야 하나? 이건 어떻게 확인하지ㅋ 진짜 내머리는 장식용  
 	if(fp==NULL){
 		//스트림해제 
 		fclose(fp);
 		return -1;
 	}
-//return : 0 - backup was successfully done(파일에 systemsize,masterpassword,deliverySystem내용  쓰기)	
+	//return : 0 - backup was successfully done(파일에 systemsize,masterpassword,deliverySystem내용  쓰기)	
 	fprintf(fp,"%d %d\n", systemSize[0], systemSize[1]);
 	fprintf(fp,"%s\n", masterPassword);
 	
@@ -125,6 +129,25 @@ int str_backupSystem(char* filepath) {
 //char* filepath : filepath and name to read config parameters (row, column, master password, past contexts of the delivery system
 //return : 0 - successfully created, -1 - failed to create the system
 int str_createSystem(char* filepath) {
+	
+	
+	//읽기모드로 파일여는 스트림 생성
+	FILE *fp = fopen(filepath,"r");
+	
+	//파일 안열림 
+	if(fp==NULL){
+		//스트림해제 
+		fclose(fp);
+		return -1;
+	}
+	
+	fscanf(fp,"%d %d",&systemSize[0],&systemSize[1]);
+	fscanf(fp,"%s",masterPassword);
+	
+	deliverySystem
+	
+	
+	 
 	
 }
 
